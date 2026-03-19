ef821840e08926e4e1cec413d80687687558f515
Boris Djordjevic <dboris@gmail.com>
2026-03-19 19:53:31 +0000

    Add search-cli formula

---
+++ Formula/search-cli.rb
@@ -0,0 +1,18 @@
+class SearchCli < Formula
+  desc "Agent-friendly multi-provider search CLI (Brave, Serper, Exa, Jina, Firecrawl)"
+  homepage "https://github.com/199-biotechnologies/search-cli"
+  url "https://github.com/199-biotechnologies/search-cli.git", tag: "v0.1.0"
+  license "MIT"
+  head "https://github.com/199-biotechnologies/search-cli.git", branch: "master"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match "search 0.1.0", shell_output("#{bin}/search --version")
+    assert_match "agent-info", shell_output("#{bin}/search --help")
+  end
+end
