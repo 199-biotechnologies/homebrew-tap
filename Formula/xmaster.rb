175c263d4d9bf15340c83c3fe721b64d317b64e1
Boris Djordjevic <dboris@gmail.com>
2026-04-04 18:03:05 +0100

    xmaster 1.5.3: fix self-update, smart from: operator

--- Formula/xmaster.rb
+++ Formula/xmaster.rb
@@ -1,22 +1,22 @@
 class Xmaster < Formula
   desc "Enterprise-grade X CLI — post, reply, like, retweet, DM, search, and more"
   homepage "https://github.com/199-biotechnologies/xmaster"
-  version "1.5.2"
+  version "1.5.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/199-biotechnologies/xmaster/releases/download/v1.5.2/xmaster-aarch64-darwin"
-      sha256 "5a4ed9cf6c8007c2e66f03882cb9e3f5721cec58a08f61e506d31254103da85e"
+      url "https://github.com/199-biotechnologies/xmaster/releases/download/v1.5.3/xmaster-aarch64-darwin"
+      sha256 "18e5e297c6820fe307a4874b700bbb125cc60675423466de9dba2097ea815e41"
     else
-      url "https://github.com/199-biotechnologies/xmaster/releases/download/v1.5.2/xmaster-x86_64-darwin"
-      sha256 "31e623988342984c0d516a8432c2c955214c884841968cdad136891c2c66b7e5"
+      url "https://github.com/199-biotechnologies/xmaster/releases/download/v1.5.3/xmaster-x86_64-darwin"
+      sha256 "afb9f61df2d2e3ea2fad2c56d924bca6112bfa4252aab938cba1988e4ba64b88"
     end
   end
 
   on_linux do
-    url "https://github.com/199-biotechnologies/xmaster/releases/download/v1.5.2/xmaster-x86_64-linux"
-    sha256 "17f89308aba2e3319505c9b623f19c59bae75cb7632436ef4b16c2d56ca22444"
+    url "https://github.com/199-biotechnologies/xmaster/releases/download/v1.5.3/xmaster-x86_64-linux"
+    sha256 "760b9c8bd715d2556ced0a9b2c91422828cf19c2a2492baa5b1c58e6091372bb"
   end
 
   def install
