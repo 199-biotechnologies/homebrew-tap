42498de16b5d79e724bb61309f0a4452e5e9d2b5
Boris Djordjevic <dboris@gmail.com>
2026-04-09 06:24:15 +0100

    chore: bump mailing-list-cli to v0.3.3

--- Formula/mailing-list-cli.rb
+++ Formula/mailing-list-cli.rb
@@ -1,8 +1,8 @@
 class MailingListCli < Formula
   desc "Newsletter and mailing list management from the terminal. Built for AI agents on top of email-cli"
   homepage "https://github.com/199-biotechnologies/mailing-list-cli"
-  url "https://github.com/199-biotechnologies/mailing-list-cli/archive/refs/tags/v0.3.2.tar.gz"
-  sha256 "db26e77a23c0102cdcd75825018a3bda7080c4d220f3357e24fdfc020f8bd106"
+  url "https://github.com/199-biotechnologies/mailing-list-cli/archive/refs/tags/v0.3.3.tar.gz"
+  sha256 "8949101501c5bf25d6166a82e718b0bd577fee7259fdb2c7b1b346bd17d13669"
   license "MIT"
   head "https://github.com/199-biotechnologies/mailing-list-cli.git", branch: "main"
 
