06075f0e9a1351f6ed3d6d36f259c02282a2d5cc
Boris Djordjevic <dboris@gmail.com>
2026-04-12 05:42:06 +0100

    search-cli 0.5.1: Add agent routing for GitHub queries to gh CLI

--- Formula/search-cli.rb
+++ Formula/search-cli.rb
@@ -1,16 +1,16 @@
 class SearchCli < Formula
   desc "Agent-friendly multi-provider search CLI with email verification (12 providers, 14 modes)"
   homepage "https://github.com/199-biotechnologies/search-cli"
-  version "0.5.0"
+  version "0.5.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/199-biotechnologies/search-cli/releases/download/v0.5.0/search-aarch64-apple-darwin.tar.gz"
-      sha256 "aa1354d7a1c166d5a7b5b4fc94c3de8cb865d9762deae5fcb37f53dcc2321ac6"
+      url "https://github.com/199-biotechnologies/search-cli/releases/download/v0.5.1/search-aarch64-apple-darwin.tar.gz"
+      sha256 "9a6f746fa5acaca9ecbf2ca93a8db2be45467b8fcb29cbc48efa8765cddbca8b"
     else
-      url "https://github.com/199-biotechnologies/search-cli/releases/download/v0.5.0/search-x86_64-apple-darwin.tar.gz"
-      sha256 "6d5187f616a49328a970d254ecd05ec49b0a8a8bb74ec1c23d6a4ae768e87726"
+      url "https://github.com/199-biotechnologies/search-cli/releases/download/v0.5.1/search-x86_64-apple-darwin.tar.gz"
+      sha256 "dbecfe8d82b9ea72e18c19aaa8a04868cf3166fb765200160bfc5c9bb13fec60"
     end
   end
 
@@ -19,8 +19,7 @@ class SearchCli < Formula
   end
 
   test do
-    assert_match "search 0.5.0", shell_output("#{bin}/search --version")
+    assert_match "search 0.5.1", shell_output("#{bin}/search --version")
     assert_match "verify", shell_output("#{bin}/search --help")
   end
 end
-
